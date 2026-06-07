818c3adb552d9cdec90096fc4c48fe6d241584de
Bushra2708 <saifuddinbushra319@gmail.com>
2026-06-07 22:09:57 +0530

    ready

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
+// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
